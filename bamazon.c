ed706839879370a4516658e8e4eb696e6c8c820f
hpeura <hpeura@mail.umw.edu>
2023-02-07 15:08:53 -0500

    Began making item struct

--- bamazon.c
+++ bamazon.c
@@ -1,4 +1,27 @@
-// hello!
-// hi!
-// hi (again)
-// hello (for the 4th time)
+#include <stdio.h>
+
+
+struct item {
+	int itemnum;
+	enum category category;
+	char *name;
+	char size;
+	int quantity;
+	double cost;
+	int onsale;
+};
+
+int read_db(char *filename) {
+
+}
+
+item *add_item(int itemnum, char *category, char *name, char size, int quantity, double cost, int onsale) {
+	struct item *i = malloc(sizeof(struct item));
+	s->itemnum = itemnum;
+	s->category = c;
+	s->size = size;
+	s->quantity = quantity;
+	s->cost = cost;
+	s->onsale = onsale;
+
+}
