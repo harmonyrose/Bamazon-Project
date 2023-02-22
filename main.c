6214dd733663c209e54e117441aaa63d4a281a9b
hpeura <hpeura@mail.umw.edu>
2023-02-22 12:27:48 -0500

    Fixed add_items implementation

--- main.c
+++ main.c
@@ -68,10 +68,15 @@ int main(int argc, char **argv){
 		if (strcmp(user, "bamazon") == 0){
 
 			if (strcmp(input[0], "add") == 0) {
-
-				for (int i =0; i < num_strs; i++){
-					printf("%s ",input[i]);
-				}
+				int itemnum = atoi(input[1]);
+				char *category = input[2];
+				char *name = input[3];
+				char size = *input[4];
+				int quantity = atoi(input[5]);
+				double cost = atof(input[6]);
+				int onsale = atoi(input[7]);
+				add_item(itemnum, category, name, size, quantity, cost, onsale);
+				
 			}
 
 			else if (strcmp(input[0], "delete") == 0) {
